fea029ddc62796fca48f76dc48868a7221163370
Wayne Chen <wchen206@my.bcit.ca>
2024-02-09 00:40:57 -0800

    added pdf for resume and footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../styles/Footer.css';
+import LinkedIn from '../assets/linkedin.png';
+import Github from '../assets/git.png';
 
 const Footer = () => {
 
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Your Website Name</p>
+      <div>
+        <p>&copy; {new Date().getFullYear()} Your Website Name</p>
+      </div>
+      <div className='social-link'>
+        <img id='linkedin' src={LinkedIn}></img>
+        <img src={Github}></img>
+      </div>
     </footer>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,19 @@ import React from 'react';
 import '../styles/Home.css';
 import BannerImage from '../assets/banner.avif';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
+import ResumeIcon from '@mui/icons-material/Description';
+import Resume from '/Users/wayanchen/Desktop/waychen.com/waychen.com/src/assets/resume.pdf';
 
 const Home = () => {
   return (
     <div id="background-image" className='main' style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className='banner-text'>
         <h1>Hi, I am a developer currently looking for co-op. Feel free to contact me!</h1>
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
-          Delete
-        </Button>
-        <Button variant="contained" endIcon={<SendIcon />}>
-          Send
-        </Button>
+        <a href={Resume} target="_blank" rel="noopener noreferrer">
+          <Button color='primary' variant="contained" endIcon={<ResumeIcon />}>
+            Resume
+          </Button>
+        </a>
       </div>
       <div id='empty-holder'></div>
     </div>
